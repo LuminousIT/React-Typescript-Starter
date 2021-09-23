89ec021ece815cca77d2baa89cd94f761fb1cb48
Samuel Olumide Oluwole <oluwoleit@gmail.com>
2021-09-23 14:15:42 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+    return (
+        <div>
+            <h1> Hello, this is a react typescript project starter template</h1>
+            <p>built with love by luminous</p>
+        </div>
+    );
+}
+
+export default App;
